579ac4f8da3d27fc729526221586cf089f36fa0b
Jedidiah Weller <jedi@openforge.io>
2022-02-02 08:05:35 -0500

    setup(): adding initial angular project with ng new and generated components with cli

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ParentComponent } from './components/parent/parent.component';
+import { ChildAComponent } from './components/child-a/child-a.component';
+import { ChildBComponent } from './components/child-b/child-b.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ParentComponent,
+    ChildAComponent,
+    ChildBComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
